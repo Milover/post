367fb18a048575f309c73a6a2fb17b2bce9881b7
Philipp Milovic <milovic.ph@gmail.com>
2023-07-03 01:57:05 -0400

    Refactor and update CLI integration tests

--- cmd/run_test.go
+++ cmd/run_test.go
@@ -6,8 +6,6 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/sirupsen/logrus"
-	"github.com/sirupsen/logrus/hooks/test"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/yaml.v3"
@@ -22,97 +20,114 @@ type runTest struct {
 
 var runTests = []runTest{
 	{
-		Name:  "basic-csv",
+		Name:  "cycle-dat",
 		Error: nil,
 		Config: `
 input:
-  file: in.csv
-  fields: [time, u_max, u_x, u_y, u_z]
-  format: csv
-  format_spec:
-    has_header: true
-    delimiter: "\t"
-    comment: "#"
+  file: testdata/dat/data.dat
+  fields: [t, patch, min, max]
+  format: dat
 process:
   - type: dummy
+  - type: filter
+    type_spec:
+      filters:
+        - field: patch
+          op: '=='
+          value: patch_1
+  - type: average-cycle
+    type_spec:
+      n_cycles: 5
+      time_field: t
+  - type: expression
+    type_spec:
+      expression: 'min+8'
+      result: min
+output:
+  directory: ""
+  table_file: "cycle-dat"
+  graphs:
+    - name: "graph-cycle-dat-patch_1"
+      axes:
+        - x:
+            min: 0.0
+            max: 1.0
+            label: "$t$-axis"
+          y:
+            min: 8.0
+            max: 12.0
+            label: "$y$-axis"
+          tables:
+            - x_field: t
+              y_field: min
+              legend_entry: '$y_\text{min}$'
+            - x_field: t
+              y_field: max
+              legend_entry: '$y_\text{max}$'
+`,
+	},
+	{
+		Name:  "cycle-series",
+		Error: nil,
+		Config: `
+input:
+  file:
+  fields: [x, y]
+  format: csv
+  format_spec:
+    has_header: # set to 'true' by default
+    delimiter: # set to ',' by default
+    comment: # set to '#' by default
+  series_spec:
+    series_directory: testdata/csv_series
+    series_file: data.csv
+    series_time_name:
+process:
+  - type: dummy
+  - type: average-cycle
+    type_spec:
+      n_cycles: 4
+      time_field: 'time'
+      time_precision: # machine precision by default
+  - type: expression
+    type_spec:
+      expression: '100*y*y'
+      result: y2
+  - type: expression
+    type_spec:
+      expression: 'y*10'
+      result: y
   - type: filter
     type_spec:
       aggregation: and
       filters:
         - field: time
-          op: '<='
-          value: 4
-        - field: u_max
+          op: '>'
+          value: 0.4
+        - field: time
           op: '<'
-          value: 7
-  - type: dummy
+          value: 0.6
 output:
   directory: ""
-  table_file: "basic-csv"
+  table_file: "cycle-series-csv"
   graphs:
-    - name: "graph-csv0"
+    - name: "graph-series-csv-cycle-avg@1.5"
       axes:
         - x:
-            min: 1.0
-            max: 3.0
+            min: 0.0
+            max: 1.0
             label: "$x$-axis"
           y:
-            min: 1.0
-            max: 6.0
+            min: 0
+            max: 25
             label: "$y$-axis"
           tables:
-            - x_field: time
-              y_field: u_max
-              legend_entry: '$u_\text{max}$'
-            - x_field: time
-              y_field: u_x
-              legend_entry: "$u_x$"
-`,
-		Input: `
-time	u_max	u_x	u_y	u_z
-1	2.0	1.0	0.0	0.0
-2	4.0	2.0	0.0	0.0
-3	6.0	3.0	0.0	0.0
-4	8.0	4.0	0.0	0.0
-5	10.0	5.0	0.0	0.0
-6	12.0	6.0	0.0	0.0
-`,
-	},
-	{
-		Name:  "basic-dat",
-		Error: nil,
-		Config: `
-input:
-  file: in.dat
-  fields: [time, u_max, u_x, u_y, u_z]
-  format: dat
-output:
-  directory: ""
-  table_file: "basic-dat"
-  graphs:
-    - name: "graph-dat0"
-      axes:
-        - x:
-            min: 1.0
-            max: 3.0
-            label: "$x$-axis"
-          y:
-            min: 1.0
-            max: 6.0
-            label: "$y$-axis"
-          tables:
-            - x_field: time
-              y_field: u_max
-              legend_entry: '$u_\text{max}$'
-            - x_field: time
-              y_field: u_x
-              legend_entry: "$u_x$"
-`,
-		Input: `
-# time	u_max	u
-1	2.0	(1.0 0.0 0.0)
-2	4.0	(2.0 0.0 0.0)
-3	6.0	(3.0 0.0 0.0)
+            - x_field: x
+              y_field: y
+              legend_entry: '$10y$'
+            - x_field: x
+              y_field: y2
+              legend_entry: '$100y^2$'
 `,
 	},
 }
@@ -122,11 +137,8 @@ func TestRun(t *testing.T) {
 		t.Run(tt.Name, func(t *testing.T) {
 			assert := assert.New(t)
 
-			var config Config
-			config.Log, _ = test.NewNullLogger()
-			config.Log.SetLevel(logrus.DebugLevel)
-
 			// read config
+			var config Config
 			raw, err := io.ReadAll(strings.NewReader(tt.Config))
 			assert.Nil(err, "unexpected io.ReadAll() error")
 			err = yaml.Unmarshal(raw, &config)
@@ -139,20 +151,12 @@ func TestRun(t *testing.T) {
 			assert.Nil(err, "unexpected os.File.Write() error")
 			defer conf.Close()
 
-			// write input
-			in, err := os.Create(config.Input.File)
-			assert.Nil(err, "unexpected os.Create() error")
-			_, err = in.WriteString(tt.Input)
-			assert.Nil(err, "unexpected os.File.Write() error")
-			defer in.Close()
-
 			t.Cleanup(func() {
 				err = os.RemoveAll(conf.Name())
 				assert.Nil(err, "unexpected os.RemoveAll() error")
-				err = os.RemoveAll(in.Name())
-				assert.Nil(err, "unexpected os.RemoveAll() error")
 			})
 
+			//logLevel = logrus.TraceLevel
 			err = run(&cobra.Command{}, []string{})
 			assert.Equal(tt.Error, err)
 		})
