043eb7cf3a599ec37ab2204ae76b679ca7b55a45
Philipp Milovic <milovic.ph@gmail.com>
2023-08-10 12:00:06 +0200

    Update run file stub

--- cmd/write_config_template.go
+++ cmd/write_config_template.go
@@ -36,18 +36,37 @@ func init() {
 const (
 	// FIXME: this should probably be automatically assembled
 	showcaseConfig string = `# run file template
-- id:                           # pipeline identifier; optional
+- id:                           # optional; pipeline identifier
   input:
-    fields: []                  # list of field names; optional
-    type:                       # one of 'dat', 'csv', 'time-series', 'ram'
+    fields: []                  # optional; list of field names
+    type:                       # one of: 'dat', 'csv', 'time-series', 'ram', 'archive', 'multiple'
    # some example type specs; there can only be 1 input type per pipeline
     type_spec:
+     # 'archive' example
+      file:                     # input archive file name; supports .tar, .tgz, .txz, .tbz, .zip
+      format_spec:              # config for an input reader, e.g., a 'csv'
+        type: csv
+        type_spec:
+          header:
+     # 'csv' example
       file:                     # input file name; usually required
-     # 'csv' specific
-      header:                   # 'true' by default
-      delimiter:                # ',' by default
-      comment:                  # '#' by default
-     # 'time-series' specific
+      header:                   # optional; 'true' by default
+      delimiter:                # optional; ',' by default
+      comment:                  # optional; '#' by default
+     # 'dat' example
+      file:                     # input file name; usually required
+     # 'multiple' example
+      format_specs:              # configs for multiple input readers, e.g., 'csv' and 'dat'
+        - type: csv
+          type_spec:
+            file:
+            header:
+        - type: dat
+          type_spec:
+            file:
+     # 'ram' example
+      name:                     # name of the data which will be accessed
+     # 'time-series' example
       directory:                # series root directory
       file:                     # series data file name
       time_name:                # 'time' by default
@@ -55,15 +74,13 @@ const (
         type: csv
         type_spec:
           header:
-     # 'ram' specific
-      name:                     # name of the data which will be accessed
   process:
    # some example processor specs, executed in order listed
     - type: average-cycle
       type_spec:
         n_cycles:
         time_field:             # if defined, turns on 'time-matching'
-        time_precision:         # machine precision by default
+        time_precision:         # optional; machine precision by default
     - type: filter
       type_spec:
         aggregation:            # one of 'and', 'or'; by default 'or'
@@ -75,25 +92,32 @@ const (
       type_spec:
         expression:             # an arithmetic expression using constants and field names
         result:                 # name of the resulting field
+    - type: select
+      type_spec:
+        fields:                 # list of field (column) names to extract
+    - type: resample
+      type_spec:
+        n_points:               # number of resampling data points
+        x_field:                # optional; indepentent variable field name
   output:
    # some example specs
     - type: ram
       type_spec:
-        name:                   # name of the data which will be stored
+        name:                   # key name under which data will be stored
     - type: csv
       type_spec:
         file:                   # output file name
-        enforce_extension:      # force correct file extension; by default 'false'
+        enforce_extension:      # optional; force correct file extension, by default 'false'
   graph:
     type:                       # only 'tex' currently
     graphs:
       - name:                   # used as a basename for all graph related files
-        directory:              # output directory name, created if not present
+        directory:              # optional; output directory name, created if not present
         table_file:             # optional; needed if 'tables.table_file' is undefined
-        template_directory:     # template directory; optional
-        template_main:          # root template file name; optional
-        template_delims:        # go template delimiters; ['__{','}__'] by default; optional
-        tex_command:            # 'pdflatex' by default; optional
+        template_directory:     # optional; template directory
+        template_main:          # optional; root template file name
+        template_delims:        # optional; go template delimiters; ['__{','}__'] by default
+        tex_command:            # optional; 'pdflatex' by default
         axes:
           - x:
               min:
@@ -110,8 +134,8 @@ const (
             tables:
               - x_field:
                 y_field:
-                col_sep:        # optional; 'comma' by default
                 legend_entry:   # raw TeX
+                col_sep:        # optional; 'comma' by default
                 table_file:     # optional; needed if 'graphs.table_file' is undefined
 `
 )
