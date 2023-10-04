2c71e8e26fb59117bbde31c7119b26ddfd99bc66
Philipp Milovic <milovic.ph@gmail.com>
2023-10-04 14:28:55 +0200

    Process input when testing Template.Execute

--- internal/template/template_test.go
+++ internal/template/template_test.go
@@ -1,6 +1,7 @@
 package template
 
 import (
+	"bytes"
 	"errors"
 	"testing"
 
@@ -251,7 +252,7 @@ func TestNodeToTemplate(t *testing.T) {
 type executeTemplateTest struct {
 	Name   string
 	Error  error
-	Input  Template
+	Input  string
 	Output string
 }
 
@@ -259,19 +260,17 @@ var executeTemplateTests = []executeTemplateTest{
 	{
 		Name:  "good",
 		Error: nil,
-		Input: Template{
-			Params: map[string][]string{
-				"x": {"0"},
-			},
-			Src: `
-input: 
-  type: ram
-  type_spec:
-    name: '{{.x}}'
+		Input: `
+- template:
+    params:
+      x: ['0']
+    src: |
+      input: 
+        type: ram
+        type_spec:
+          name: '{{.x}}'
 `,
-		},
-		Output: `
-input: 
+		Output: `input: 
   type: ram
   type_spec:
     name: '0'
@@ -280,34 +279,32 @@ input:
 	{
 		Name:  "good-realistic",
 		Error: nil,
-		Input: Template{
-			Params: map[string][]string{
-				"name":  {"a"},
-				"value": {"0", "1"},
-			},
-			Src: `
-- input: 
-    type: ram
-    type_spec:
-      name: '{{.name}}'
-  process:
-  - type: expression
-    type_spec:
-      expression: 'phase-{{.value}}'
-      result: phase
-  - *phase_filter
-  - type: regexp-rename
-    type_spec:
-      src: '(.*)'
-      result: '${1}_{{.value}}'
-  output:
-  - type: ram
-    type_spec:
-      name: 'rans_{{.name}}'
+		Input: `
+- template:
+    params:
+      name: [a]
+      value: ['0', '1']
+    src: |
+      - input: 
+          type: ram
+          type_spec:
+            name: '{{.name}}'
+        process:
+        - type: expression
+          type_spec:
+            expression: 'phase-{{.value}}'
+            result: phase
+        - *phase_filter
+        - type: regexp-rename
+          type_spec:
+            src: '(.*)'
+            result: '${1}_{{.value}}'
+        output:
+        - type: ram
+          type_spec:
+            name: 'rans_{{.name}}'
 `,
-		},
-		Output: `
-- input: 
+		Output: `- input: 
     type: ram
     type_spec:
       name: 'a'
@@ -325,7 +322,6 @@ input:
   - type: ram
     type_spec:
       name: 'rans_a'
-
 - input: 
     type: ram
     type_spec:
@@ -353,9 +349,22 @@ func TestExecuteTemplate(t *testing.T) {
 		t.Run(tt.Name, func(t *testing.T) {
 			assert := assert.New(t)
 
-			out, err := tt.Input.Execute()
+			var n yaml.Node
+			err := yaml.Unmarshal([]byte(tt.Input), &n)
+			assert.Nil(err, "unexpected yaml.Unmarshal() error")
 
-			assert.Equal(tt.Output, string(out))
+			found, err := findNodes(&n)
+			assert.Nil(err, "unexpected findNodes() error")
+			var b bytes.Buffer
+			for _, f := range found {
+				tmpl, err := nodeToTemplate(f)
+				assert.Nil(err, "unexpected nodeToTemplate() error")
+				out, errf := tmpl.Execute()
+				errors.Join(err, errf)
+				_, errb := b.Write(out)
+				assert.Nil(errb, "unexpected nodeToTemplate() error")
+			}
+			assert.Equal(tt.Output, b.String())
 			assert.ErrorIs(err, tt.Error)
 		})
 	}
